b4366146122bdd84e0c294355dcc6632a03f75ef
KoheiSuda <sudakohei201@gmail.com>
2025-11-05 17:31:17 +0900

    fix circuit

--- src/qsvt/lse_solver.py
+++ src/qsvt/lse_solver.py
@@ -1,7 +1,6 @@
 import numpy as np
 from inverse_matrix import compute_matrix_inverse_qsvt, generate_angles_qsvt_and_scale
 from qiskit import QuantumCircuit
-from qiskit_aer import Aer
 from qiskit.quantum_info import Statevector
 
 SHOTS = 8192
@@ -19,13 +18,6 @@ class LSESolver:
         self.A = np.asarray(A, dtype=complex)
         self.b = np.asarray(b, dtype=complex)
         self.n, self.m = self.A.shape
-
-        self.frobenius_norm = None
-        self.A_normalized = None
-        self.normalize_matrix()
-
-    def normalize_matrix(self):
-        # NOTE: 行列Aの特異値が1以下になるように正規化。
         self.frobenius_norm = np.linalg.norm(self.A, ord="fro")
         self.A_normalized = self.A / self.frobenius_norm
 
@@ -40,9 +32,7 @@ class LSESolver:
         A_inv = np.linalg.inv(self.A)
         x_solution = A_inv @ self.b
 
-        residual_norm = np.linalg.norm(self.A @ x_solution - self.b)
-
-        return {"solution": x_solution, "residual_norm": residual_norm}
+        return x_solution
 
     def solve_linear_system_quantum(self):
         """
@@ -64,52 +54,60 @@ class LSESolver:
         angles_qsvt, s = generate_angles_qsvt_and_scale(kappa)
 
         full_unitary, qsvt_circuit, encoding_wires, _ = compute_matrix_inverse_qsvt(
-            self.A_normalized,  angles_qsvt
+            self.A_normalized, angles_qsvt
         )
         print(f"full_unitary:\n{np.round(full_unitary, 4)}")
         print(f"scale: {s}")
 
         n_sys = int(np.ceil(np.log2(len(self.b))))
-        sys_wires = list[int, ...](range(1, n_sys + 1))
+        sys_wires = list[int, ...](range(n_sys))
+        print("sys_wires:", sys_wires)
 
         # 3) 合成用の土台回路は “QSVT回路と同じ本数” にする
         combined_circuit = QuantumCircuit(qsvt_circuit.num_qubits)
 
         # 4) |b> を “系” の量子ビットにだけ初期化
-        combined_circuit.initialize(b_normalized, sys_wires)
+        combined_circuit.initialize(b_normalized, 0)
         init_state = Statevector.from_instruction(combined_circuit)
         print("Initial statevector before QSVT:")
         print(np.round(init_state.data, 4))
 
-        # 5) QSVT を合成（qubit 並びが一致していればOK）
+        # # 5) QSVT を合成（qubit 並びが一致していればOK）
         combined_circuit.compose(qsvt_circuit, inplace=True)
+        print("Statevector after QSVT:", Statevector.from_instruction(combined_circuit))
 
-        # Step 5: 測定で解xを取得
-        # システムレジスタのみ測定（アンシラは無視）
-        combined_circuit.measure_all()
+        # # Step 5: 測定で解xを取得
+        # # システムレジスタのみ測定（アンシラは無視）
+        # combined_circuit.measure_all()
 
-        # Step 6: 実行と後処理
-        backend = Aer.get_backend(SIMULATOR)
-        result = backend.run(combined_circuit, shots=SHOTS).result()
-        counts = result.get_counts()
-        print("Measurement results:")
-        print(counts)
+        # # Step 6: 実行と後処理
+        # backend = Aer.get_backend(SIMULATOR)
+        # result = backend.run(combined_circuit, shots=SHOTS).result()
+        # counts = result.get_counts()
+        # print("Measurement results:", counts)
 
-        # Step 7: 測定結果から解ベクトルを再構成
-        x_solution = self._reconstruct_from_counts(counts)
+        # # Step 7: 測定結果から解ベクトルを再構成
+        # x_solution = self._reconstruct_from_counts(counts)
+
+        x_solution = self.measure_real_amplitudes(qsvt_circuit, b_normalized)
+        print("x_solution before scaling:", x_solution)
+
         # スケールを調整
         x_solution = x_solution * b_norm / (s * self.frobenius_norm)
 
         return x_solution
 
+    def measure_real_amplitudes(self, qsvt_circuit, b_normalized):
+        pass
+
     def _reconstruct_from_counts(self, counts):
         """
-        現在の配置（|anc>|b> の順＝qubit0=anc、qubit1=system）に対応。
+        現在の配置（|anc>|b> の順＝qubit0=b、qubit1=0）に対応。
         anc=0 のときの条件付き確率から振幅の絶対値を復元。
         """
         # anc=0のカウントだけ抜く
-        # NOTE: Qiskitの仕様上最初のビットが最右に記載される。
-        post_counts = {bits: c for bits, c in counts.items() if bits[-1] == "0"}
+        post_counts = {bits: c for bits, c in counts.items() if bits[0] == "0"}
+        print("post_counts:", post_counts)
         total_post = sum(post_counts.values())
 
         if total_post == 0:
@@ -118,7 +116,7 @@ class LSESolver:
         # systemの確率
         p_sys = [0.0, 0.0]
         for bits, c in post_counts.items():
-            sys_bit = bits[0]  # 左側がsystem
+            sys_bit = bits[-1]  # 右側がsystem
             p_sys[int(sys_bit)] += c
 
         # 条件付き確率に正規化
@@ -143,11 +141,6 @@ class LSESolver:
         print(f"QSVT:      {np.round(qsvt_result, 4)}")
 
 
-def qsvt_solve_linear_system(A, b):
-    lse_solver = LSESolver(A=A, b=b)
-    return lse_solver.solve_linear_system_quantum()
-
-
 if __name__ == "__main__":
     A = np.array([3, 1, 1, 3]).reshape((2, 2))
     b = np.array([1, 2], dtype="complex")
@@ -164,5 +157,12 @@ if __name__ == "__main__":
 
     # b = np.array([1, 2, 3, 4], dtype="complex")
 
-    x_solution = qsvt_solve_linear_system(A, b)
+    print(f"A:\n{np.round(A, 4)}")
+    print(f"b:\n{np.round(b, 4)}")
+
+    lse_solver = LSESolver(A=A, b=b)
+
+    x_solution = lse_solver.solve_linear_system_quantum()
     print(f"x_solution: {np.round(x_solution, 4)}")
+    x_solution_classical = lse_solver.solve_lse_classical()
+    print(f"x_solution_classical: {np.round(x_solution_classical, 4)}")
